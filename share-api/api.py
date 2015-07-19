53ea1ad42c81147494583d41312465e9702dcc65
Vignesh Venkataraman <technovigno@gmail.com>
2015-07-19 06:23:28 -0700

    adding a debug line

--- share-api/api.py
+++ share-api/api.py
@@ -33,6 +33,7 @@ def insert_entity_data(entity_id):
 		entity_id = uuid.uuid4()
 	entity_id = str(entity_id).lower()
 	entity_id = entity_id.replace('_', ' ')
+	print request.data
 	json_data = json.loads("{ \""+ entity_id + "\": " + request.data + "}")
 	print json_data
 	try:
